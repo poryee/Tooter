1c725e35380591668a08bb0f577973465951f6e4
poryee <poryee1@hotmail.com>
2016-09-16 17:35:19 +0800

    connection

--- connectDB.php
+++ connectDB.php
@@ -10,6 +10,26 @@ $user = "tooteradmin";
 $pwd = "Tt123456";
 $db = "tooterdb";
 
+/*
+Server: tooterserver.database.windows.net,1433 \r\n
+SQL Database: tooterdb
+User Name: tooteradmin\r\n\r\n
+PHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   
+*/
+try{
+    $conn = new PDO ("sqlsrv:server = tcp:tooterserver.database.windows.net,1433; Database = tooterdb", tooteradmin, Tt123456);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) 
+{
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+/**        
+$connectionInfo = array(UID => tooteradmin@tooterserver, pwd => Tt123456, Database => tooterdb, LoginTimeout => 30, Encrypt => 1, TrustServerCertificate => 0);
+$serverName = tcp:tooterserver.database.windows.net,1433;
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 // Connecting to database
 try {
     $connection = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
@@ -19,3 +39,5 @@ catch(Exception $e){
     die(var_dump($e));
 }
 
+*/
+?>
